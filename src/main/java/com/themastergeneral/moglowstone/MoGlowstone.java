06d62e9763f9a01858d933ffb66c0ffefd8a9b8d
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-03-10 20:22:50 -0500

    Changes to update json

--- src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
+++ src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
@@ -20,10 +20,10 @@ public class MoGlowstone
 
     public static final String MODID = "moglowstone";
     public static final String MODNAME = "Mo' Glowstone";
-    public static final String VERSION = "1.2.3";
+    public static final String VERSION = "1.2.4";
     public static final String acceptedMinecraftVersions = "1.10.2";
     public static final String DEPENDENCIES = "required-after:ctdcore@[1.0.2,];";
-    public static final String updateJSON = "https://dl.dropboxusercontent.com/u/72961306/TMG%20Assets/Update%20JSONs/Mo-Glowstone.json";
+    public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/Mo-Glowstone.json";
     
     public static final CreativeTab creativeTab = new CreativeTab();
     
