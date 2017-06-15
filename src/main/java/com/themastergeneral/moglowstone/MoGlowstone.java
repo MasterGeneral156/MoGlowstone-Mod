57526304c84e904b156bea621a695f544dced030
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-06-15 17:52:16 -0400

    New CTD Core, update forge

--- src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
+++ src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
@@ -20,9 +20,9 @@ public class MoGlowstone
 
     public static final String MODID = "moglowstone";
     public static final String MODNAME = "Mo' Glowstone";
-    public static final String VERSION = "1.2.6";
+    public static final String VERSION = "1.2.7";
     public static final String acceptedMinecraftVersions = "1.12";
-    public static final String DEPENDENCIES = "required-after:ctdcore@[1.1.0,];";
+    public static final String DEPENDENCIES = "required-after:ctdcore@[1.1.2,];";
     public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/Mo-Glowstone.json";
     
     public static final CreativeTab creativeTab = new CreativeTab();
