0f8b0ca7b0ec45329bb0d18e598addac2431be6a
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-10-16 11:34:07 -0400

    1.4.2

--- src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
+++ src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
@@ -20,7 +20,7 @@ public class MoGlowstone {
 
 	public static final String MODID = "moglowstone";
 	public static final String MODNAME = "Mo' Glowstone";
-	public static final String VERSION = "1.4.1";
+	public static final String VERSION = "1.4.2";
 	public static final String DEPENDENCIES = "required-after:ctdcore@[1.2,];";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/Mo-Glowstone.json";
 	public static final String acceptedMinecraftVersions = "1.12.2";
