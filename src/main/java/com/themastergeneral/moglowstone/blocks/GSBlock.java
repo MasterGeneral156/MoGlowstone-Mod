b5adea1956f0a4c30586bcce83ec391bad384f45
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-01-08 13:22:49 -0500

    Implement lighting fixes on the correct class lol

--- src/main/java/com/themastergeneral/moglowstone/blocks/GSBlock.java
+++ src/main/java/com/themastergeneral/moglowstone/blocks/GSBlock.java
@@ -7,7 +7,11 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Loader;
 
 import com.themastergeneral.ctdcore.block.CTDBlock;
 import com.themastergeneral.moglowstone.MoGlowstone;
@@ -18,12 +22,14 @@ public class GSBlock extends CTDBlock {
 	public String color;
 	public EnumDyeColor dyecolor;
 
-	protected GSBlock(String name, String modid, String color, EnumDyeColor dyecolor) {
+	protected GSBlock(String name, String modid, String color,
+			EnumDyeColor dyecolor) {
 
 		super(Material.GLASS, name, modid);
 		this.setCreativeTab(MoGlowstone.creativeTab);
-		this.setLightLevel(1.0F);
-		this.setHardness(0.3F);
+		if (!Loader.isModLoaded("albedo")) {
+			this.setLightLevel(1.0F);
+		}
 		this.setSoundType(blockSoundType.GLASS);
 		this.color = color;
 		this.dyecolor = dyecolor;
@@ -39,5 +45,13 @@ public class GSBlock extends CTDBlock {
 	public TileEntity createTileEntity(@Nonnull World world,
 			@Nonnull IBlockState state) {
 		return new TEGlowstone();
+	}
+
+	@Override
+	public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
+		if (Loader.isModLoaded("Albedo"))
+			if (FMLCommonHandler.instance().getEffectiveSide().isServer())
+				return 15;
+		return super.getLightValue(state, world, pos);
 	}
 }
