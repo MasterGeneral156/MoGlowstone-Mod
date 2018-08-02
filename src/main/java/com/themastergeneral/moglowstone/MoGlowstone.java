08f348bb884eec869462e1893a857b03c6ee9f77
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2018-08-02 17:40:47 -0400

    New jar key, because new PC

--- src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
+++ src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
@@ -20,11 +20,11 @@ public class MoGlowstone {
 
 	public static final String MODID = "moglowstone";
 	public static final String MODNAME = "Mo' Glowstone";
-	public static final String VERSION = "1.5.4";
+	public static final String VERSION = "1.5.5";
 	public static final String DEPENDENCIES = "required-after:ctdcore@[1.2,];after:albedo";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/Mo-Glowstone.json";
 	public static final String acceptedMinecraftVersions = "1.12.2";
-	public static final String Fingerprint = "5101015479fe39f20b47f365472250d312a50a57";
+	public static final String Fingerprint = "b50fe22651b9d97d9d6631514a6a57cd6174b0dc";
 
 	public static final CreativeTab creativeTab = new CreativeTab();
 
