00ba677a669f95a7a9807e08e8ce7258aa2ec14a
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-02-20 17:52:45 -0500

    Blocks registed :D

--- src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
+++ src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
@@ -1,60 +1,74 @@
 package com.themastergeneral.moglowstone;
 
-import net.minecraftforge.fml.common.FMLLog;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.themastergeneral.moglowstone.proxy.CommonProxy;
-import com.themastergeneral.moglowstone.proxy.client.CreativeTab;
+import com.themastergeneral.moglowstone.blocks.ModBlocks;
 
-@Mod(modid = MoGlowstone.MODID, name = MoGlowstone.MODNAME, certificateFingerprint = MoGlowstone.Fingerprint, version = MoGlowstone.VERSION, dependencies = MoGlowstone.DEPENDENCIES, updateJSON = MoGlowstone.updateJSON, acceptedMinecraftVersions = MoGlowstone.acceptedMinecraftVersions)
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.IForgeRegistry;
+
+@Mod("moglowstone")
 public class MoGlowstone {
+	
+	public static MoGlowstone instance;
+	private static final Logger LOGGER = LogManager.getLogger();
 
 	public static final String MODID = "moglowstone";
-	public static final String MODNAME = "Mo' Glowstone";
-	public static final String VERSION = "1.5.6";
-	public static final String DEPENDENCIES = "required-after:ctdcore@[1.2,];after:albedo";
-	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/Mo-Glowstone.json";
-	public static final String acceptedMinecraftVersions = "1.12.2";
-	public static final String Fingerprint = "b50fe22651b9d97d9d6631514a6a57cd6174b0dc";
 
-	public static final CreativeTab creativeTab = new CreativeTab();
+	public MoGlowstone() {
+		instance = this;
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
-	@Instance
-	public static MoGlowstone instance = new MoGlowstone();
-	public static Logger logger;
-
-	@SidedProxy(clientSide = "com.themastergeneral.moglowstone.proxy.ClientProxy", serverSide = "com.themastergeneral.moglowstone.proxy.ServerProxy")
-	public static CommonProxy proxy;
-
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent e) {
-		logger = e.getModLog();
-		logger.info("Mo' Glowstone is gonna brighten your world up!");
-		proxy.preInit(e);
-	}
-
-	@EventHandler
-	public void init(FMLInitializationEvent e) {
-		proxy.init(e);
-	}
-
-	@EventHandler
-	public void postInit(FMLPostInitializationEvent e) {
-		proxy.postInit(e);
-		logger.info("Your world wills shine bright now, thanks to Mo' Glowstone!");
-	}
-
-	@EventHandler
-	public void onFingerprintViolation(FMLFingerprintViolationEvent e) {
-		FMLLog.warning("Invalid fingerprint detected for Mo' Glowstone! TheMasterGeneral will not support this version!");
-	}
+        // Register ourselves for server, registry and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+	
+	private void setup(final FMLCommonSetupEvent event)
+    {
+		LOGGER.info("Mo' Glowstone is launching.");
+    }
+	
+	@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class Registration
+    {
+		@SubscribeEvent
+        public static void registerBlocks(final RegistryEvent.Register<Block> event)
+        {
+			IForgeRegistry<Block> blockRegistry = event.getRegistry();
+			
+			blockRegistry.register(ModBlocks.black_gsblock);
+			blockRegistry.register(ModBlocks.blue_gsblock);
+			blockRegistry.register(ModBlocks.brown_gsblock);
+			blockRegistry.register(ModBlocks.brick_gsblock);
+			blockRegistry.register(ModBlocks.cyan_gsblock);
+			blockRegistry.register(ModBlocks.gray_gsblock);
+			blockRegistry.register(ModBlocks.green_gsblock);
+			blockRegistry.register(ModBlocks.lamp_gsblock);
+			blockRegistry.register(ModBlocks.lblue_gsblock);
+			blockRegistry.register(ModBlocks.lgray_gsblock);
+			blockRegistry.register(ModBlocks.lime_gsblock);
+			blockRegistry.register(ModBlocks.magenta_gsblock);
+			blockRegistry.register(ModBlocks.orange_gsblock);
+			blockRegistry.register(ModBlocks.red_gsblock);
+			blockRegistry.register(ModBlocks.pink_gsblock);
+			blockRegistry.register(ModBlocks.purple_gsblock);
+			blockRegistry.register(ModBlocks.white_gsblock);
+			blockRegistry.register(ModBlocks.glowstone_ore);
+        }
+		
+		@SubscribeEvent
+        public static void registerItems(final RegistryEvent.Register<Item> event)
+        {
+			IForgeRegistry<Item> itemRegistry = event.getRegistry();
+        }
+    }
 }
