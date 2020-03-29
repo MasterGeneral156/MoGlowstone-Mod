38cd6b2805886fb0a5b1c23978d22e61129bee8f
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-03-29 13:17:54 -0400

    Imrpove the ore-dict.

--- src/main/java/com/themastergeneral/moglowstone/handler/OreDict.java
+++ src/main/java/com/themastergeneral/moglowstone/handler/OreDict.java
@@ -1,5 +1,6 @@
 package com.themastergeneral.moglowstone.handler;
 
+import net.minecraft.init.Blocks;
 import net.minecraftforge.oredict.OreDictionary;
 
 import com.themastergeneral.moglowstone.blocks.ModBlocks;
@@ -21,5 +22,38 @@ public class OreDict {
 		OreDictionary.registerOre("glowstone", ModBlocks.purple_gsblock);
 		OreDictionary.registerOre("glowstone", ModBlocks.red_gsblock);
 		OreDictionary.registerOre("glowstone", ModBlocks.white_gsblock);
+		
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.black_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.blue_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.brown_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.cyan_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.gray_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.green_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.lblue_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.lgray_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.lime_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.magenta_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.orange_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.pink_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.purple_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.red_gsblock);
+		OreDictionary.registerOre("glowstoneColored", ModBlocks.white_gsblock);
+		
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.black_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.blue_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.brown_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.cyan_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.gray_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.green_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.lblue_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.lgray_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.lime_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.magenta_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.orange_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.pink_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.purple_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.red_gsblock);
+		OreDictionary.registerOre("blockGlowstone", ModBlocks.white_gsblock);
+		OreDictionary.registerOre("blockGlowstone", Blocks.GLOWSTONE);
 	}
 }
