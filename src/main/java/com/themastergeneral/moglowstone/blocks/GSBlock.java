a6688fac5d485f9de17735ebe94678dc7e1b537d
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-11-07 17:21:02 -0500

    This *might* fix my issue.

--- src/main/java/com/themastergeneral/moglowstone/blocks/GSBlock.java
+++ src/main/java/com/themastergeneral/moglowstone/blocks/GSBlock.java
@@ -4,9 +4,12 @@ import com.themastergeneral.ctdcore.block.CTDBlock;
 import com.themastergeneral.moglowstone.MoGlowstone;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraftforge.common.ToolType;
 
 public class GSBlock extends CTDBlock {
@@ -16,10 +19,15 @@ public class GSBlock extends CTDBlock {
 		super(Block.Properties.create(Material.GLASS, MaterialColor.SAND)
 				.sound(SoundType.GLASS)
 				.hardnessAndResistance(0.3F)
-				.setLightLevel(s -> 15)
 				.harvestTool(ToolType.PICKAXE),
 				name, 
 				MoGlowstone.MODID);
+	}
+	
+	@Override
+	public int getLightValue(BlockState state, IBlockReader world, BlockPos pos)
+	{
+		return 15;
 	}
 
 }
--- src/main/java/com/themastergeneral/moglowstone/blocks/GSOre.java
+++ src/main/java/com/themastergeneral/moglowstone/blocks/GSOre.java
@@ -4,9 +4,12 @@ import com.themastergeneral.ctdcore.block.CTDBlock;
 import com.themastergeneral.moglowstone.MoGlowstone;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraftforge.common.ToolType;
 
 public class GSOre extends CTDBlock {
@@ -18,5 +21,11 @@ public class GSOre extends CTDBlock {
 				.harvestTool(ToolType.PICKAXE)
 				.harvestLevel(3),
 				name, MoGlowstone.MODID);
+	}
+	
+	@Override
+	public int getLightValue(BlockState state, IBlockReader world, BlockPos pos)
+	{
+		return 2;
 	}
 }
