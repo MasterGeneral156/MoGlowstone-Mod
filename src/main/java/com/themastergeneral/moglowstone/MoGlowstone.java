7c873599899b22b23d9b9201eaa2dfd4ab19393f
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-12-29 22:43:33 -0500

    Fix tab?

--- src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
+++ src/main/java/com/themastergeneral/moglowstone/MoGlowstone.java
@@ -31,10 +31,18 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.themastergeneral.moglowstone.blocks.BlockRegistry;
+import com.themastergeneral.moglowstone.blocks.ModBlocks;
 import com.themastergeneral.moglowstone.items.ItemRegistry;
+import com.themastergeneral.moglowstone.items.ModItems;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -46,22 +54,61 @@ public class MoGlowstone {
 	static final Logger LOGGER = LogManager.getLogger();
 
 	public static final String MODID = "moglowstone";
-	public static final CreativeModeTab ITEMGROUP = new MoGlowstoneItemGroup();
+	
+	public static CreativeModeTab MoGlowstoneTab;
 
 	public MoGlowstone() {
 		instance = this;
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
+        modbus.addListener(this::setup);
+        modbus.addListener(this::registerTabs);
+        modbus.addListener(this::fillTab);
 
         // Register ourselves for server, registry and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
         
-        ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        BlockRegistry.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ItemRegistry.ITEMS.register(modbus);
+        BlockRegistry.BLOCKS.register(modbus);
     }
 	
 	private void setup(final FMLCommonSetupEvent event)
     {
 		LOGGER.info("Mo' Glowstone is launching.");
     }
+	
+	private void registerTabs(CreativeModeTabEvent.Register event)
+    {
+		MoGlowstoneTab = event.registerCreativeModeTab(new ResourceLocation(MODID, "moglowstone_tab"), builder -> builder
+                .icon(() -> new ItemStack(ModItems.red_glowstone_block))
+                .title(Component.translatable("tabs.modid.main_tab"))
+                .displayItems((featureFlags, output, hasOp) -> { }));
+    }
+	
+	@SubscribeEvent
+	private void fillTab(CreativeModeTabEvent.BuildContents ev)
+	{
+		if (ev.getTab() == MoGlowstoneTab)
+		{
+			ev.accept(ModItems.glowstone_coal);
+			ev.accept(ModBlocks.black_glowstone_block);
+			ev.accept(ModBlocks.brown_glowstone_block);
+			ev.accept(ModBlocks.blue_glowstone_block);
+			ev.accept(ModBlocks.brick_glowstone_block);
+			ev.accept(ModBlocks.cyan_glowstone_block);
+			ev.accept(ModBlocks.glowstone_ore);
+			ev.accept(ModBlocks.gray_glowstone_block);
+			ev.accept(ModBlocks.green_glowstone_block);
+			ev.accept(ModBlocks.lamp_glowstone_block);
+			ev.accept(ModBlocks.light_gray_glowstone_block);
+			ev.accept(ModBlocks.light_blue_glowstone_block);
+			ev.accept(ModBlocks.lime_glowstone_block);
+			ev.accept(ModBlocks.magenta_glowstone_block);
+			ev.accept(ModBlocks.orange_glowstone_block);
+			ev.accept(ModBlocks.pink_glowstone_block);
+			ev.accept(ModBlocks.purple_glowstone_block);
+			ev.accept(ModBlocks.red_glowstone_block);
+			ev.accept(ModBlocks.white_glowstone_block);
+		}
+	}
 }
