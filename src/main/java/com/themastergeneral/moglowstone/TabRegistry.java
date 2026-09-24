dfcccc2ecbb0306a1366c5132ca23a0030e08085
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2026-09-24 09:06:05 -0500

    Updated file copyright

--- src/main/java/com/themastergeneral/moglowstone/TabRegistry.java
+++ src/main/java/com/themastergeneral/moglowstone/TabRegistry.java
@@ -1,5 +1,5 @@
-/*
-	Project:	Mo' Glowstone 26.2
+/**
+	Project:	Mo' Glowstone 26.3
 	File:		com.themastergeneral.moglowstone.TabRegistry
 	Author:		TheMasterGeneral
 	Website: 	https://github.com/MasterGeneral156/MoGlowstone-Mod
